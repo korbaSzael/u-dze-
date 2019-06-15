80e7f83d74f1d9c202a10b85ba2fafe895b30fbd
korbaszael <korbaszael@o2.pl>
2019-06-15 18:22:09 +0200

    nochmal

--- gra/GlobalSuppressions.cs
+++
@@ -1,8 +0,0 @@
-﻿
-// This file is used by Code Analysis to maintain SuppressMessage 
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Styl", "IDE0017:Uprość inicjowanie obiektów", Justification = "<Oczekujące>", Scope = "member", Target = "~M:gra.Form1.#ctor")]
-
